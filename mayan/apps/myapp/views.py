6320534026e5186ca6855595554eec6410295d68
Kailash Jadhav <110094400+Kailashjadhav123@users.noreply.github.com>
2023-11-05 19:06:33 +0530

    Update views.py

--- mayan/apps/myapp/views.py
+++ mayan/apps/myapp/views.py
@@ -4,11 +4,11 @@ from .models import Document
 
 class DocumentListView(ListView):
     model = Document
-    template_name = 'listview.html'  
+    template_name = 'myapp/listview.html'  
     context_object_name = 'context'  
 
 
 class DocumentDetailview(DetailView):
     model = Document
-    template_name = 'detailview.html' 
+    template_name = 'myapp/detailview.html' 
     context_object_name = 'context'
